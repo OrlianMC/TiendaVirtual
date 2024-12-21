625102ff0092112353ab619954cab3e1a1470866
OrlianMC <orlian.mc@gmail.com>
2024-12-21 03:58:54 -0500

    seventh commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,3 @@
-// import { Link } from 'react-router-dom';
-
-// function Home() {
-//   return (
-//     <div className="flex flex-col items-center justify-center text-center min-h-screen bg-white p-4">
-//       <h1 className="text-4xl font-bold mb-4">Bienvenido a la Tienda Virtual</h1>
-//       <p className="text-lg text-center mb-6">
-//         Explora nuestros productos y encuentra lo que más te gusta.
-//       </p>
-//       <Link to="/products" className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition">
-//         Ver Productos
-//       </Link>
-//     </div>
-//   );
-// }
-
-// export default Home;
-
 import { Link } from 'react-router-dom';
 
 function Home() {
